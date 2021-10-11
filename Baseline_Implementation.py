48333764f8dbac71ff8097b40790b65ff10b5972
Noel Alben <noelalben21@gmail.com>
2021-10-11 11:14:35 -0400

    Code to work off of

---
+++ Baseline_Implementation.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy
+import matplotlib as plt
+import pandas as pd
+
+def Onset_detection():
+    return ("Onset Information")
+
+def Spec_Crust_Tonalness():
+    return("Give tonal vs ATONAL between inter onsets")
+
+def f0_detection():
+    return("Find F0 in tonal section and return the samples to next onset of note")
+
+def some_visualization():
+    return("Time vs note re[presentation on a graph")
+
+def evalutation():
+    return("Check Evaluation on datasets")
+    
