7649251f778bc9dec3a14de135e226d5efc56165
thiago.roque <thiago.roque@grupoicts.com.br>
2021-12-15 13:01:12 -0400

    fast commit prior to presentation

--- Pitch_Detection/pitch_tracking_evaluation.py
+++ Pitch_Detection/pitch_tracking_evaluation.py
@@ -1,333 +1,158 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 09:59:42 2021
+
+@author: thiag
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
 import os
 import glob
-import math
-import numpy as np
-import scipy as sp
-from scipy import signal
-import pandas as pd
+import Onsetdet as onset
+import Pitch_Tracking as pitch
+# import time
 
-import matplotlib.pyplot as plt
-from scipy.io.wavfile import read as wavread
+def convert_freq2midi(freqInHz):
+    a0 = 440
+    pitchInMIDI = 69 + 12 * np.log2(freqInHz/a0)
+    return pitchInMIDI
 
-def ToolReadAudio(cAudioFilePath):
-    [samplerate, x] = wavread(cAudioFilePath)
+def eval_voiced_fp(estimation, annotation):
+    return np.count_nonzero(estimation[np.where(annotation == 0)[0]])/np.where(annotation == 0)[0].size
 
-    if x.dtype == 'float32':
-        audio = x
-    else:
-        # change range to [-1,1)
-        if x.dtype == 'uint8':
-            nbits = 8
-        elif x.dtype == 'int16':
-            nbits = 16
-        elif x.dtype == 'int32':
-            nbits = 32
+def eval_voiced_fn(estimation, annotation):
+    return np.where(estimation[np.nonzero(annotation)] == 0)[0].size/np.count_nonzero(annotation)
 
-        audio = x / float(2 ** (nbits - 1))
+def eval_pitchtrack_v2(estimation, annotation):
+    estimateInCents = 100 * convert_freq2midi(estimation[np.intersect1d(np.nonzero(estimation), np.nonzero(annotation))])
+    groundtruthInCents = 100 * convert_freq2midi(annotation[np.intersect1d(np.nonzero(estimation), np.nonzero(annotation))])
+    errCentRms = np.sqrt(np.mean((estimateInCents-groundtruthInCents)**2))
+    return errCentRms, eval_voiced_fp(estimation, annotation), eval_voiced_fn(estimation, annotation)
 
-    # special case of unsigned format
-    if x.dtype == 'uint8':
-        audio = audio - 1.
-
-    return samplerate, audio
-
-def block_audio(x, blockSize, hopSize, fs):
-    """
-    reference code from Audio Content Analysis course
-    """
-    # allocate memory
-    numBlocks = int(np.ceil(x.size / hopSize))
-    xb = np.zeros([numBlocks, blockSize])
-
-    t = (np.arange(0, numBlocks) * hopSize) / fs
-    x = np.concatenate((x, np.zeros(blockSize)), axis=0)
-    for n in range(0, numBlocks):
-        i_start = n * hopSize
-        i_stop = np.min([x.size - 1, i_start + blockSize - 1])
-
-        xb[n][np.arange(0, blockSize)] = x[np.arange(i_start, i_stop + 1)]
-
-    return xb, t
-
-
-
-# def thiago_get_f0_from_acf (r, fs):
+def run_evaluation_Bach(pathAudioFolder, pathGTFolder):
+    blockSize = 1024 
+    hopSize = 128
+    thresholdDb = -40
+    audioFileList = glob.glob(pathAudioFolder +'/'+'*.wav')
+    GTFileList = glob.glob(pathGTFolder +'/'+'*.csv')
+    estimateInHz = np.array([])
+    groundtruthInHz = np.array([])
     
-#     peakind, _ = signal.find_peaks(r, distance=30)
-#     highPeakind = np.argmax(r[peakind])
-    
-#     t0 = peakind[highPeakind]
-#     print(t0)
-#     f0 = -1
-#     try:
-#         if(t0 != 0):
-#             f0 = fs/t0
-#     except:
-#         print(f"t0 value is: {t0}")
-#         f0 = -1
-#     return f0
+    for idx, audioFile in enumerate(audioFileList):
+        # startTime = time.time()
+        fs, x = pitch.ToolReadAudio(audioFile)
+        csv = np.genfromtxt(GTFileList[idx], delimiter=",")
+        gtF0 = csv[:,1]
+        [f0,timeInSec] = pitch.track_pitch_nccf_eval(x, blockSize, hopSize, fs, thresholdDb)
+        interpolatedF0 = pitch.interpolateSize(f0,gtF0.shape[0])
+        
+        errCentRms, pfp, pfn = eval_pitchtrack_v2(interpolatedF0, gtF0)
+        displayname = GTFileList[idx].split('\\')[-1]
+        print(f"RMS error for {displayname} is {errCentRms:.2f} cents. False positive is {pfp*100:2f}%. False negtative is {pfn*100:2f}%.")
+        
+        estimateInHz = np.concatenate((estimateInHz, interpolatedF0), 0)
+        groundtruthInHz = np.concatenate((groundtruthInHz, gtF0), 0)
+        # executionTime = (time.time() - startTime)
+        # print('Execution time in seconds: ' + str(executionTime))
+    overallErrCentRms, overallPfp, overallPfn = eval_pitchtrack_v2(estimateInHz, groundtruthInHz)
+    print(overallErrCentRms)
+    print(overallPfp)
+    print(overallPfn)
+    return overallErrCentRms, overallPfp, overallPfn
+    # return interpolatedF0, gtF0
 
-def get_f0_from_acf (r, fs):
-    eta_min = 1
-    afDeltaCorr = np.diff(r)
-    eta_tmp = np.argmax(afDeltaCorr > 0)
-    eta_min = np.max([eta_min, eta_tmp])
-    f = np.argmax(r[np.arange(eta_min + 1, r.size)])
-    f = fs / (f + eta_min + 1)
-    return (f)
+def run_evaluation_Flute(fullpath_txt,fullpath_aud):
+    blockSizeOnset = 512 
+    hopSizeOnset = 256
+    thresholdDb = -20
+    txt = []
+    for file in os.listdir(fullpath_txt):
+            if (file.startswith("scale") & file.endswith('.txt')):
+                txt.append(fullpath_txt+file)
+    aud = []
+    for file in os.listdir(fullpath_aud):
+            if (file.startswith("scale") & file.endswith('.wav')):
+                aud.append(fullpath_aud+file)
+    for i,k in zip(txt,aud):
+        gnd = np.genfromtxt(i,skip_header=4,skip_footer=1)
+        gnd_onset = np.genfromtxt(i,skip_header=4,skip_footer=1)
+        if (gnd.size == 0):
+            with open(i) as f:
+                lines = np.array(f.readlines())
+                lines = lines[4:-1]
+                tmp = [data.split(' ') for data in lines]
+                gnd_onset = [i[0] for i in tmp]
+                gnd = [i[2][:-1] for i in tmp]
+                gnd = np.asarray(gnd).astype('float32')
+                gnd_onset = np.asarray(gnd_onset).astype('float32')
+        else:        
+            gnd = gnd[:,2]
+            gnd_onset = gnd_onset[:,0]
+        fs, x = pitch.ToolReadAudio(k)
+        peakInSamples = onset.onset_detect(x,blockSizeOnset,hopSizeOnset,fs)
+        midiNoteArray,f0 = pitch.onset_note_tracking(x, np.int32(peakInSamples), fs, thresholdDb)
+        print(gnd)
+        print(f0)
+    return f0, gnd
 
 
+def run_evaluation(path):
+    filelist = glob.glob(path+'/'+'*.wav')
+    estimateInHz = np.array([])
+    groundtruthInHz = np.array([])
+    for files in filelist:
+        fs, x = pitch.ToolReadAudio(files)
+        filename = files.split('.')[0]
+        txt = glob.glob(filename+'*.txt')
+        data = open(txt[0])
+        txt_read = np.loadtxt(data)
+        f0, timeInSec = pitch.track_pitch_nccf(x, 1024, 512, fs, -40)
+        errCentRms, pfp, pfn = eval_pitchtrack_v2(f0, txt_read[:, 2].T)
+        displayname = files.split('\\')[-1]
+        print(f"RMS error for {displayname} is {errCentRms:.2f} cents. False positive is {pfp*100:2f}%. False negtative is {pfn*100:2f}%.")
+        plt.figure()
+        plt.plot(timeInSec, f0,'.')
+        plt.plot(txt_read[:, 0], txt_read[:, 2])
+        plt.xlabel('Time/s')
+        plt.ylabel('Frequency/Hz')
+        plt.title(f'Detected Frequency for {displayname}')
+        plt.legend(['Detected', 'Ground Truth'])
+        plt.show()
+        estimateInHz = np.concatenate((estimateInHz, f0), 0)
+        groundtruthInHz = np.concatenate((groundtruthInHz, txt_read[:, 2].T), 0)
+    return eval_pitchtrack_v2(estimateInHz, groundtruthInHz)
 
 
-
-
-def get_f0_from_nsdf (d, fs):
-    
-    peakind, _ = signal.find_peaks(d, distance=30)
-    if (peakind.size!=0):
-        highPeakind = np.argmax(d[peakind])
-        t0 = peakind[highPeakind]
-        f0 = fs/t0
-    else:
-        f0 = 0 
-    
-    return f0
-
-
-def mod_acf2(x):
-#     print(f"within mod_acf2: x={x}")
-    try:
-        if(np.max(x)):
-            x=x/np.max(x)
-    except:
-        print("fata")
-        x = 0
-    x[x > 0] = 1
-    x[x < 0] = -1
-    x[x == 0] = 0
-    return x
-
-def extract_rms(xb):
-    rmsDb = np.zeros(xb.shape[0])
-    for block in range(xb.shape[0]):
-        rmsDb[block] = np.sqrt(np.mean(np.square(xb[block])))
-        threshold = 1e-5  # truncated at -100dB
-        if rmsDb[block] < threshold:
-            rmsDb[block] = threshold
-        rmsDb[block] = 20 * np.log10(rmsDb[block])
-    return rmsDb
-
-def convert_freq2midi(fInHz, fA4InHz = 440):
-        def convert_freq2midi_scalar(f, fA4InHz):
-            if f <= 0:
-                return 0
-            else:
-                return (69 + 12 * np.log2(f/fA4InHz))
-            
-        fInHz = np.asarray(fInHz)
-        if fInHz.ndim == 0:
-            return convert_freq2midi_scalar(fInHz,fA4InHz)
-        midi = np.zeros(fInHz.shape)
-        for k,f in enumerate(fInHz):
-            midi[k] =  convert_freq2midi_scalar(f,fA4InHz)
-#             print("midi[k]:",midi[k])    
-        return (midi)
-    
-def eval_pitchtrack(estimateInHz, groundtruthInHz):
-        if np.abs(groundtruthInHz).sum() <= 0:
-            return 0
-        # truncate longer vector
-        if groundtruthInHz.size < estimateInHz.size:
-            print("within if")
-            estimateInHz = estimateInHz[np.arange(0,groundtruthInHz.size)]
-        elif estimateInHz.size < groundtruthInHz.size:
-            print("within else")
-            groundtruthInHz = groundtruthInHz[np.arange(0,estimateInHz.size)]
-        diffInCent = 100*(convert_freq2midi(estimateInHz) - convert_freq2midi(groundtruthInHz))
-        print(f"est: {estimateInHz}, gt: {groundtruthInHz}, diffincent:{diffInCent}")
-        rms = np.sqrt(np.mean(diffInCent**2))
-        return (rms)
-    
-def eval_pitchtrack_v2(estimation,annotation): 
-    errCentRms= eval_pitchtrack(estimation, annotation)
-    return errCentRms
-
-def comp_nccf(inputVector):
-    
-    inputLength = len(inputVector)
-    n = np.ones(inputLength)
-    paddedInput = np.pad(inputVector, (0,inputLength-1))
-    for i in range(inputLength):        
-        n[i] = np.sqrt(np.sum(inputVector**2)*np.sum((paddedInput[i:i+inputLength])**2))
-        if n[i] == 0:
-            n[i] = 1
-    print(n)
-    return n
-
-def comp_FFTacf(inputVector):
-    
-    inputLength = len(inputVector)
-    inFFT = np.fft.fft(inputVector, 2*inputLength)
-    S = np.conj(inFFT)*inFFT
-    c_fourier = np.real(np.fft.ifft(S))
-    # c_fourier = (np.fft.ifft(S))
-    r_fft = c_fourier[:(c_fourier.size//2)]
-    
-    return r_fft
-
-def get_f0_from_nccf (r, n, fs):
-    
-    nccf = r/n
-    peakind, _ = signal.find_peaks(nccf, distance=30)
-    print("peakind:", peakind)
-    if (peakind.size!=0):
-        highPeakind = np.argmax(nccf[peakind])
-        t0 = peakind[highPeakind]
-        f0 = fs/t0
-    else:
-        f0 = 0
-    return f0
-
-def track_pitch_nccf(x,blockSize,hopSize,fs): 
-    
-    [xb, timeInSec] = block_audio(x,blockSize,hopSize,fs)
-    f0 = np.zeros(len(timeInSec))    
-    for idx, val in enumerate(xb):
-        mod_val = mod_acf2(val) 
-        n= comp_nccf(mod_val)
-        r = comp_FFTacf(mod_val)
-        print("Wthn track_pitch_nccf r:", r)
-        f0[idx] = get_f0_from_nccf (r, n, fs)
-    print(f"nccf: f0 {f0}")
+def run_on_file(path, blockSize, hopSize):
+    fs, x = pitch.ToolReadAudio(path)
+    f0, timeInSec = pitch.track_pitch_nccf(x, blockSize, hopSize, fs, -30)
+    plt.figure()
+    plt.plot(f0)    
     return [f0,timeInSec]
 
-def get_d_from_r_m(r,m):
-    return ((2*r)/m)
 
-def comp_Msdf(inputVector):
-    inputLength = len(inputVector)
-    m = np.zeros(inputLength)
-    paddedInput = np.pad(inputVector, (0,inputLength-1))
-    for i in range(inputLength):        
-        m[i] = np.sum((inputVector**2)+(paddedInput[i:i+inputLength])**2)
-    return m
-
-def track_pitch_nsdf(x,blockSize,hopSize,fs): 
+# just for testing
+if __name__ == '__main__':
     
-    [xb, timeInSec] = block_audio(x,blockSize,hopSize,fs)
-    f0 = np.zeros(len(timeInSec))    
-    for idx, val in enumerate(xb):
-        
-        mod_val = mod_acf2(val) 
-        m = comp_Msdf(mod_val)
-        r = comp_acf(mod_val,False)
-        d = get_d_from_r_m(r,m)
-        f0[idx] = get_f0_from_nsdf (d, fs)
-    print(f"nsdf: f0 {f0}")
-    return [f0,timeInSec]
-  
-# def thiago_comp_acf(inputVector, bIsNormalized):
-#     inputLength = len(inputVector)
-#     r = np.zeros(inputLength)
-#     paddedInput = np.pad(inputVector, (0,inputLength-1))
-#     for i in range(inputLength):
-#         r[i] = np.dot(inputVector, paddedInput[i:i+inputLength])
+    # # Run Evaluation for the Bach dataset # #
+    pathGTBachFolder = 'C:/Users/thiag/Documents/Github/melograph/Bach10-mf0-synth/annotation_stems'
+    pathAudioBachFolder = 'C:/Users/thiag/Documents/Github/melograph/Bach10-mf0-synth/audio_stems'
+    [overallErrCentRms, overallPfp, overallPfn] = run_evaluation_Bach(pathAudioBachFolder,pathGTBachFolder)
+   
+    # # Run Evaluation for the Flute dataset # #
+    # pathAudioFluteFolder = '../melograph/flute-audio-labelled-database-AMT/'
+    # pathGTFluteFolder = '../melograph/flute-audio-labelled-database-AMT/'
+    # run_evaluation_Flute(pathGTFluteFolder,pathAudioFluteFolder)
     
-#     if bIsNormalized == True:
-#         if (np.dot(inputVector,inputVector)!= 0):
-#             r = r/np.dot(inputVector,inputVector)
-# #         else:
-# #             print("comp_acf r=", r)
-            
-#     return r
-
-def comp_acf(inputVector, bIsNormalized = True):
-    if bIsNormalized:
-        norm = np.dot(inputVector, inputVector)
-    else:
-        norm = 1
-    afCorr = np.correlate(inputVector, inputVector, "full") / norm
-    afCorr = afCorr[np.arange(inputVector.size-1, afCorr.size)]
-    return (afCorr)
+    # # Run whole process for single file # #
+    # pathAudioSolo = 'C:/Users/thiag/Documents/Github/melograph/Bach10-mf0-synth/audio_stems/01_AchGottundHerr_clarinet.RESYN.wav'
+    # blockSize = 1024
+    # hopSize = 256
+    # thresholdDb = -20
+    # fs, x = pitch.ToolReadAudio(pathAudioSolo)
+    # peakInSamples = onset.onset_detect(x,blockSize,hopSize,fs)
+    # midiNoteArray,f0 = pitch.onset_note_tracking(x, np.int32(peakInSamples), fs, thresholdDb)
 
 
-
-def track_pitch_acf(x,blockSize,hopSize,fs):
-    
-    # get blocks
-    [xb,t] = block_audio(x,blockSize,hopSize,fs)
-    # init result
-    f0 = np.zeros(xb.shape[0]
-                  )
-    # compute acf
-    for n in range(0, xb.shape[0]):
-        r = comp_acf(xb[n,:])
-        
-        f0[n] = get_f0_from_acf(r,fs)
-    return (f0,t)
-
-def track_pitch(x,blockSize,hopSize,fs,method):
-    
-    if method == 'acf':
-        f0, timeInSec = track_pitch_acf(x,blockSize, hopSize, fs)
-    elif method == 'nccf':
-        f0, timeInSec = track_pitch_nccf(x,blockSize, hopSize, fs)
-    elif method == 'nsdf':
-        f0, timeInSec = track_pitch_nsdf(x,blockSize, hopSize, fs)
-    """
-    xb, t = block_audio(x, blockSize, hopSize, fs)
-    rmsDb = extract_rms(xb)
-    voicing_mask = create_voicing_mask(rmsDb, voicingThres)
-    f0Adj = apply_voicing_mask(f0, voicing_mask)
-    
-    return f0Adj, timeInSec
-    """
-    return f0, timeInSec
-
-def evaluate_trackpitch(pathToAudio, pathToGT):
-    methods = ['acf','nccf','nsdf']
-    iNumOfFiles = 0
-    rms_avg = np.zeros((3,2))
-    for met in range(len(methods)):
-        rmsAvg = 0
-        pfp=0
-        pfn=0
-        pfp_sum = 0
-        pfn_sum = 0
-        rmsAvg_sum = 0
-
-        wav_file_names = [os.path.join(pathToAudio, _) for _ in os.listdir(pathToAudio) if _[-4:] == ".wav"]
-        csv_file_names = [os.path.join(pathToGT, _) for _ in os.listdir(pathToGT) if _[-4:] == ".csv"]
-        i = 0
-        for wav,csv in zip(wav_file_names, csv_file_names):
-            i+=1
-            print(f'{i}, processing {wav} and {csv}')
-
-            fs, x = ToolReadAudio(wav)
-            f0Adj, timeInSec = track_pitch(x, 128, 128, fs, methods[met])
-            
-            df = pd.read_csv(csv)
-            df.columns = ["time", "f0"]
-            
-            rmsAvg = eval_pitchtrack_v2(f0Adj, df['f0'])
-            rmsAvg_sum += rmsAvg
-
-        rms_avg[met] = rmsAvg_sum/len(csv_file_names)
-    return rms_avg
-
-#testing one by one
-
-pathToAudio = "/Users/rhythmjain/Desktop/GTStuff/1-2/AudioContent/melograph/Bach10-mf0-synth/audio_stems/"
-
-wav_file_names = [os.path.join(pathToAudio, _) for _ in os.listdir(pathToAudio) if _[-4:] == ".wav"]
-fs, x = ToolReadAudio(wav_file_names[0])
-track_pitch_acf(x, 128, 128, fs)
-# track_pitch_nccf(x, 128, 128, fs)
-# track_pitch_nsdf(x, 128, 128, fs)
-
-#testing all together
-
-path_audio = "/Users/rhythmjain/Desktop/GTStuff/1-2/AudioContent/melograph/Bach10-mf0-synth/audio_stems/"
-path_anno = "/Users/rhythmjain/Desktop/GTStuff/1-2/AudioContent/melograph/Bach10-mf0-synth/annotation_stems"
-# evaluate_trackpitch(path_audio, path_anno)
-
+# EOF
--- WebApp/functions.py
+++ WebApp/functions.py
@@ -34,14 +34,23 @@ def ToolReadAudio(cAudioFilePath):
 
     return samplerate, audio
 
+def ToolBlockAudio(afAudioData, iBlockLength, iHopLength):
+
+    iNumOfBlocks = np.floor((afAudioData.shape[0] - iBlockLength) / iHopLength + 1).astype(int)
+
+    if iNumOfBlocks < 1:
+        return np.array([])
+    return np.vstack([np.array(afAudioData[i*iHopLength:i*iHopLength+iBlockLength]) for i in range(iNumOfBlocks)])
+
 def block_audio(x,blockSize,hopSize,fs):
     
     inLen = len(x)
     nBlock = int(np.ceil((inLen-blockSize)/hopSize)+1)
+    # nBlock = int(np.ceil(inLen/hopSize))
     xb = np.zeros((nBlock,blockSize))
     timeInSample = np.arange(0, hopSize*nBlock, hopSize)
     timeInSec = timeInSample/fs
-    for i in range(len(timeInSec)):
+    for i in range(nBlock):
         if i == len(timeInSec)-1:
             zeroPad = blockSize - len(x[int(timeInSample[i]):])
             xb[i] = np.pad(x[int(timeInSample[i]):], (0,zeroPad))
@@ -51,7 +60,7 @@ def block_audio(x,blockSize,hopSize,fs):
   
 
 def mod_acf2(x):
-    x = x/np.max(np.abs(x))
+    # x = x/np.max(np.abs(x))
     x[x > 0] = 1
     x[x < 0] = -1
     x[x == 0] = 0
@@ -73,6 +82,10 @@ def comp_FFTnccf(inputVector):
     n = np.sqrt(comp_FFTacf(sqInput))
     return n
 
+def smooth(x, kernelSize):
+    kernel = np.ones(kernelSize)/kernelSize
+    return np.convolve(x, kernel, mode='same')
+
 def interpolate(y, padding):
     ySize = y.shape[0]
     x = np.linspace(0, ySize, num=ySize, endpoint=True)
@@ -81,9 +94,18 @@ def interpolate(y, padding):
     y_interpolated = f(xnew)
     return y_interpolated
 
+def interpolateSize(y, newSize):
+    ySize = y.shape[0]
+    x = np.linspace(0, ySize, num=ySize, endpoint=True)
+    f = interp1d(x, y, kind='quadratic')
+    xnew = np.linspace(0, ySize, num=newSize, endpoint=True)
+    y_interpolated = f(xnew)
+    y_interpolated[y_interpolated < 5] = 0
+    return y_interpolated
+
 def get_f0_from_nccf(r, n, fs):
     nccf = r/n
-    peakind_nccf, _ = signal.find_peaks(nccf, distance=50, prominence=10)
+    peakind_nccf, _ = signal.find_peaks(nccf, distance=50, prominence=20, threshold=1)
     if (peakind_nccf.shape[0] != 0):
         highPeakind_nccf = np.argmax(nccf[peakind_nccf])
         targetIndex = peakind_nccf[highPeakind_nccf]
@@ -150,7 +172,7 @@ def peak_picking_adaptive_threshold(x, median_len=16, offset_rel=0.05, sigma=4.0
 def Onset_detection(path):
     fs,x = ToolReadAudio(path)
     nov,scale_factor = Novelty_HPSS_Spectral(x,fs)
-    peaks = peak_picking_adaptive_threshold(nov, median_len=16, offset_rel=0.05, sigma=4.0)
+    peaks = peak_picking_adaptive_threshold(nov, median_len=16, offset_rel=0.01, sigma=4.0)
     # peaks = np.int32(np.around(peaks*scale_factor*256*2))
     scale_factor = x.size/nov.size
     peaks = np.int32(np.around(peaks*scale_factor))
@@ -168,6 +190,7 @@ def extract_rms_chunk(x):
 def track_pitch_nccf(x,blockSize,hopSize,fs, thresholdDb): 
     
     xb = block_audio(x,blockSize,hopSize,fs)
+    xb.T[xb.T == 0] = 0.00001
     f0 = np.zeros(xb.shape[0])    
     for idx, val in enumerate(xb):
         
@@ -178,21 +201,39 @@ def track_pitch_nccf(x,blockSize,hopSize,fs, thresholdDb):
 
     f0[f0>2000] = 0
     
-    _f0 = f0.copy()
-    comparison = f0.copy()
-    for i in reversed(range(1, f0.shape[0]-1)):
-        if comparison[i] < 40:
-            comparison[i] = comparison[i+1]
-    # Smoothing and compare to eliminate some occasional octave errors
-    smooth_over = 20
-    comparison = np.convolve(comparison, np.ones(smooth_over * 2 + 1) / (smooth_over * 2 + 1), mode='valid')
-    for i in range(smooth_over, _f0.shape[0] - smooth_over):
-        if abs(_f0[i]/2-comparison[i - smooth_over]) < abs(_f0[i] - comparison[i - smooth_over]):
-            _f0[i] = _f0[i]/2
-        if abs(_f0[i]*2-comparison[i - smooth_over]) < abs(_f0[i] - comparison[i - smooth_over]):
-            _f0[i] = _f0[i]*2
-                
-    return _f0
+    smoothOver = 20
+    # _f0 = f0.copy()
+    # comparison = f0.copy()
+    # for i in reversed(range(1, f0.shape[0]-1)):
+    #     if comparison[i] < 40:
+    #         comparison[i] = comparison[i+1]
+    ## Smoothing and compare to eliminate some occasional octave errors
+    # comparison = np.convolve(comparison, np.ones(smoothOver * 2 + 1) / (smoothOver * 2 + 1), mode='valid')
+    # for i in range(smoothOver, _f0.shape[0] - smoothOver):
+    #     if abs(_f0[i]/2-comparison[i - smoothOver]) < abs(_f0[i] - comparison[i - smoothOver]):
+    #         _f0[i] = _f0[i]/2
+    #     if abs(_f0[i]*2-comparison[i - smoothOver]) < abs(_f0[i] - comparison[i - smoothOver]):
+    #         _f0[i] = _f0[i]*2
+    
+    smoothedF0 = smooth(f0, smoothOver)
+    rmsDb = extract_rms(xb)
+    mask = create_voicing_mask(rmsDb, thresholdDb)
+    masked_f0 = apply_voicing_mask(smoothedF0, mask)
+    
+    return masked_f0
+
+def extract_rms(xb):
+    return np.clip((10*np.log10(np.mean(xb**2, axis=1))), -100, 0)
+
+def create_voicing_mask(rmsDb, thresholdDb):
+    mask = np.zeros(rmsDb.shape[0])
+    mask[np.where(rmsDb > thresholdDb)] = 1
+    # mask[np.where(flux < fluxThreshold)] = 1
+    return mask
+    
+
+def apply_voicing_mask(f0, mask):
+    return f0 * mask
 
 def freq2MIDI(f0,a0):
     f0_ = np.copy(f0)
@@ -203,6 +244,7 @@ def freq2MIDI(f0,a0):
     f0_ = np.delete(f0,popIdx.astype(int))
     pitchInMIDI = np.round(69 + 12 * np.log2(f0_/a0))
     return pitchInMIDI
+
 
 def onset_note_tracking(x, oInBlocks, fs, rmsThreshold):
     smallBlockSize = 512
